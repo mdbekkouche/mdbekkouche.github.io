4e436516ba98123a0e2d6a780d55a25f8df62554
BEKKOUCHE Mohammed <mdbekkouche@gmail.com>
2024-11-29 18:47:18 +0100

    Add faulty programs used in experiments

---
+++ Benchmarks_LocFaults/Programs/TriPerimetrev1.java
@@ -0,0 +1,68 @@
+package benchmark;
+
+public class TriPerimetrev1 {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int triperimetre (int i, int j, int k) {
+		int trityp = 0;
+		int res;
+		if (i == 0 || j == 0 || k == 0) {
+			trityp = 4;	
+			res = -1;
+		}
+		else {
+			trityp = 0;
+			if (i == j) {
+				trityp = trityp + 1;
+			}
+			if (i == k) {
+				trityp = trityp + 2;
+			}
+			if (j == k) {
+				trityp = trityp + 3;
+			}
+			if (trityp == 0) {
+				if ((i+j) <= k || ((j+k) <= i || (i+k) <= j)) {
+					trityp = 4;
+					res = -1;
+				}
+				else {
+					trityp = 1;
+					res = i+j+k;
+				}			
+			}
+			else {
+				if (trityp > 3) {
+					res = 3*i;
+				}
+				else {
+					if (trityp == 1 && (i+j) > k) { // i==j
+						res=2*i+k; 
+					}
+					else {
+						// error in the assignment : should be res = 2*i+j
+						if (trityp == 2 && (i+k) > j) {    // i==k
+							res = 2*i + k;						
+						}
+						else {
+							if (trityp == 3 && (j+k) > i) {   // j==k
+								res=2*j+i;
+							}
+							else {
+								res=-1;
+							}
+						}
+					}
+				}
+			}
+		}
+		return res;
+	}
+}
